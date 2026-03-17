bed028a5ef39c715a718acd7e4e1fb6b54733706
David Bankson <62538124+QuothTheRaven42@users.noreply.github.com>
2026-03-16 21:45:53 -0700

    Day 2 of scrapy project
    https://www.youtube.com/watch?v=mBoX_JCKZTE

--- settings.py
+++ settings.py
@@ -3,9 +3,16 @@ BOT_NAME = 'bookscraper'
 SPIDER_MODULES = ['bookscraper.spiders']
 NEWSPIDER_MODULE = 'bookscraper.spiders'
 
+# FEEDS = {
+#     'booksdata.csv': {'format': 'csv'}
+# }
+
+
+
 # obey robots.txt rule
 ROBOTSTXT_OBEY = True
 
 ITEM_PIPELINES = {
     'bookscraper.pipelines.BookscraperPipeline': 300,
+    # 'bookscraper.pipelines.SaveToMysqlPipeline': 400,
 }
